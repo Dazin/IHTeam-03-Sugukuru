4ad5bbb063bc15531ff158ab18aeed575cd29db6
Tatsuya <das.egalite@gmail.com>
2018-11-26 16:50:01 +0900

    回収途中

--- Sugukuru/Kaishu.Designer.cs
+++ Sugukuru/Kaishu.Designer.cs
@@ -31,7 +31,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
-            this.btOpen = new System.Windows.Forms.Button();
+            this.btLoadZengin = new System.Windows.Forms.Button();
             this.btLoadSeikyu = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -52,6 +52,7 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(25, 63);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.RowTemplate.Height = 21;
             this.dataGridView1.Size = new System.Drawing.Size(956, 444);
@@ -66,15 +67,15 @@
             this.button1.Text = "一括消し込みする";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // btOpen
+            // btLoadZengin
             // 
-            this.btOpen.Location = new System.Drawing.Point(255, 25);
-            this.btOpen.Name = "btOpen";
-            this.btOpen.Size = new System.Drawing.Size(103, 23);
-            this.btOpen.TabIndex = 4;
-            this.btOpen.Text = "全銀ファイル読込";
-            this.btOpen.UseVisualStyleBackColor = true;
-            this.btOpen.Click += new System.EventHandler(this.btOpen_Click);
+            this.btLoadZengin.Location = new System.Drawing.Point(255, 25);
+            this.btLoadZengin.Name = "btLoadZengin";
+            this.btLoadZengin.Size = new System.Drawing.Size(103, 23);
+            this.btLoadZengin.TabIndex = 4;
+            this.btLoadZengin.Text = "全銀ファイル読込";
+            this.btLoadZengin.UseVisualStyleBackColor = true;
+            this.btLoadZengin.Click += new System.EventHandler(this.btLoadZengin_Click);
             // 
             // btLoadSeikyu
             // 
@@ -84,7 +85,7 @@
             this.btLoadSeikyu.TabIndex = 5;
             this.btLoadSeikyu.Text = "請求情報読込";
             this.btLoadSeikyu.UseVisualStyleBackColor = true;
-            this.btLoadSeikyu.Click += new System.EventHandler(this.button2_Click);
+            this.btLoadSeikyu.Click += new System.EventHandler(this.btLoadSeikyu_Click);
             // 
             // Kaishu
             // 
@@ -92,7 +93,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1007, 569);
             this.Controls.Add(this.btLoadSeikyu);
-            this.Controls.Add(this.btOpen);
+            this.Controls.Add(this.btLoadZengin);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
@@ -110,7 +111,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button btOpen;
+        private System.Windows.Forms.Button btLoadZengin;
         private System.Windows.Forms.Button btLoadSeikyu;
     }
 }
--- Sugukuru/Kaishu.cs
+++ Sugukuru/Kaishu.cs
@@ -15,6 +15,7 @@ namespace Sugukuru
     public partial class Kaishu : Form
     {
         String conStr;
+        Boolean flag = false;
         public Kaishu()
         {
             InitializeComponent();
@@ -34,96 +35,13 @@ namespace Sugukuru
             dataGridView1.Columns.Add("mikaishu", "未回収金額");
             dataGridView1.Columns[6].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
             dataGridView1.Columns.Add("status", "状態");
-            dataGridView1.Rows.Add("10001", "太郎", "080-XXXX-XXXX",  "100,000", "30年11月27日", "20,000", "80,000", "未回収");
+//            dataGridView1.Rows.Add("10001", "太郎", "080-XXXX-XXXX",  "100,000", "30年11月27日", "20,000", "80,000", "未回収");
         }
 
 
 
-        private void btOpen_Click(object sender, EventArgs e)
-        {
-            //OpenFileDialogクラスのインスタンスを作成
-            OpenFileDialog ofd = new OpenFileDialog();
 
-            //はじめのファイル名を指定する
-            //はじめに「ファイル名」で表示される文字列を指定する
-//            ofd.FileName = "default.html";
-            ofd.FileName = "";
-            //はじめに表示されるフォルダを指定する
-            //指定しない（空の文字列）の時は、現在のディレクトリが表示される
-            ofd.InitialDirectory = @"C:\";
-            //[ファイルの種類]に表示される選択肢を指定する
-            //指定しないとすべてのファイルが表示される
-            ofd.Filter = "HTMLファイル(*.html;*.htm)|*.html;*.htm|すべてのファイル(*.*)|*.*";
-            //[ファイルの種類]ではじめに選択されるものを指定する
-            //2番目の「すべてのファイル」が選択されているようにする
-            ofd.FilterIndex = 2;
-            //タイトルを設定する
-            ofd.Title = "開くファイルを選択してください";
-            //ダイアログボックスを閉じる前に現在のディレクトリを復元するようにする
-            ofd.RestoreDirectory = true;
-            //存在しないファイルの名前が指定されたとき警告を表示する
-            //デフォルトでTrueなので指定する必要はない
-            ofd.CheckFileExists = true;
-            //存在しないパスが指定されたとき警告を表示する
-            //デフォルトでTrueなので指定する必要はない
-            ofd.CheckPathExists = true;
-
-            //ダイアログを表示する
-            if (ofd.ShowDialog() == DialogResult.OK)
-            {
-                //OKボタンがクリックされたとき、選択されたファイル名を表示する
-                Console.WriteLine(ofd.FileName); //ファイル名出力
-
-
-                //OKボタンがクリックされたとき、選択されたファイルを読み取り専用で開く
-                System.IO.Stream stream;
-                stream = ofd.OpenFile();
-                if (stream != null)
-                {
-                    //内容を読み込み、表示する
-                    System.IO.StreamReader sr =
-                        new System.IO.StreamReader(stream
-                        ,System.Text.Encoding.GetEncoding("shift_jis") //文字コード指定して読み込み
-                        );
-                    //                    Console.WriteLine(sr.ReadToEnd()); //全件一括表示
-
-                    int iDataType = 0;
-                    StringBuilder strOutput = new StringBuilder();
-                    while (sr.EndOfStream == false) //一行ずつ読み込む
-                    {
-                        string line = sr.ReadLine();
-                        Console.WriteLine(line);
-
-                        if (int.TryParse(line.Substring(0, 1), out iDataType))
-                        {
-                            switch (iDataType)
-                            {
-                                case 2:                                    //データレコード
-                                    int[] fWidth = new int[] { 1, 6, 6, 6, 10, 10, 10, 48, 15, 15, 1, 20, 52 };
-                                    String[] sWidth = new String[] { "データ区分", "照会番号", "勘定日", "起算日", "金額", "うち他店券金額", "振込依頼人コード", "振込依頼人名", "仕向銀行名", "仕向支店名", "取消区分", "EDI情報", "ダミー" };
-                                    int i = 0;
-                                    int linenum = 0;
-                            foreach (int val in fWidth)
-                                    {
-                                    Console.WriteLine(sWidth[i].ToString() + ":" + line.Substring(linenum, val));
-                                        linenum += val; //現在位置をプラスする。
-                                        ++i;
-                                    }
-                                    break;
-                                default:
-                                    break;
-                            }
-                        }
-                    }
-                    sr.Close();
-                    stream.Close();
-                }
-
-            }
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
+        private void btLoadSeikyu_Click(object sender, EventArgs e)
         {
             // DGVの行を空にする。
             dataGridView1.Rows.Clear();
@@ -170,6 +88,127 @@ namespace Sugukuru
                 }
                 dataGridView1.Rows.Add(kCode, kName, kTel, sPrice, nyukinDate , nyukinGaku, mikaishu, status);
             }
+        }
+
+
+        private void btLoadZengin_Click(object sender, EventArgs e)
+        {
+            //OpenFileDialogクラスのインスタンスを作成
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            //はじめのファイル名を指定する
+            //はじめに「ファイル名」で表示される文字列を指定する
+            //            ofd.FileName = "default.html";
+            ofd.FileName = "入金インポートサンプル.txt";
+            //はじめに表示されるフォルダを指定する
+            //指定しない（空の文字列）の時は、現在のディレクトリが表示される
+            ofd.InitialDirectory = @"C:\";
+            //[ファイルの種類]に表示される選択肢を指定する
+            //指定しないとすべてのファイルが表示される
+            ofd.Filter = "HTMLファイル(*.html;*.htm)|*.html;*.htm|すべてのファイル(*.*)|*.*";
+            //[ファイルの種類]ではじめに選択されるものを指定する
+            //2番目の「すべてのファイル」が選択されているようにする
+            ofd.FilterIndex = 2;
+            //タイトルを設定する
+            ofd.Title = "開くファイルを選択してください";
+            //ダイアログボックスを閉じる前に現在のディレクトリを復元するようにする
+            ofd.RestoreDirectory = true;
+            //存在しないファイルの名前が指定されたとき警告を表示する
+            //デフォルトでTrueなので指定する必要はない
+            ofd.CheckFileExists = true;
+            //存在しないパスが指定されたとき警告を表示する
+            //デフォルトでTrueなので指定する必要はない
+            ofd.CheckPathExists = true;
+
+            //ダイアログを表示する
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                //OKボタンがクリックされたとき、選択されたファイル名を表示する
+                Console.WriteLine(ofd.FileName); //ファイル名出力
+
+
+                //OKボタンがクリックされたとき、選択されたファイルを読み取り専用で開く
+                System.IO.Stream stream;
+                stream = ofd.OpenFile();
+                if (stream != null)
+                {
+                    //内容を読み込み、表示する
+                    System.IO.StreamReader sr =
+                        new System.IO.StreamReader(stream
+                        , System.Text.Encoding.GetEncoding("shift_jis") //文字コード指定して読み込み
+                        );
+                    //                    Console.WriteLine(sr.ReadToEnd()); //全件一括表示
+
+                    int iDataType = 0;
+                    StringBuilder strOutput = new StringBuilder();
+
+
+                    //テーブルの中身を空にする。
+                    MySqlConnection con = new MySqlConnection(this.conStr);
+                    // コマンドを作成
+                    MySqlCommand cmd = new MySqlCommand("TRUNCATE TABLE 全銀", con);
+                    // オープン
+                    cmd.Connection.Open();
+                    // 実行
+                    cmd.ExecuteNonQuery();
+                    // クローズ
+                    cmd.Connection.Close();
+                    //テーブルの中身を空にする。 ここまで
+
+                    while (sr.EndOfStream == false) //一行ずつ読み込む
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+
+                        String strInsert = "insert into 全銀 (`データ区分`, `照会番号`, `勘定日`, `起算日`, `金額`, `うち他店券金額`, `振込依頼人コード`, `振込依頼人名`, `仕向銀行名`, `仕向支店名`, `取消区分`, `EDI情報`, `ダミー`) values ("; //インサート文の一部
+                        String strInsertCheck = strInsert; //チェック用
+                        if (int.TryParse(line.Substring(0, 1), out iDataType))
+                        {
+                            switch (iDataType)
+                            {
+                                case 2:                                    //データレコード
+                                    int[] fWidth = new int[] { 1, 6, 6, 6, 10, 10, 10, 48, 15, 15, 1, 20, 52 };
+                                    String[] sWidth = new String[] { "データ区分", "照会番号", "勘定日", "起算日", "金額", "うち他店券金額", "振込依頼人コード", "振込依頼人名", "仕向銀行名", "仕向支店名", "取消区分", "EDI情報", "ダミー" };
+                                    int i = 0;//データ種類名取り出し用
+                                    int linenum = 0;//値取り出し用
+                                    foreach (int val in fWidth)
+                                    {
+                                        Console.WriteLine(sWidth[i].ToString() + ":" + line.Substring(linenum, val));
+                                        //(sWidth[i].ToString() データ種類
+                                        //line.Substring(linenum, val)); データ
+                                        if (strInsert != strInsertCheck) { strInsert += ','; }//初回以外カンマを入れる。
+                                        strInsert += '"' + line.Substring(linenum, val) + '"';
+                                        linenum += val; //現在位置をプラスする。
+                                        ++i;
+                                    }
+                                    strInsert += ")";
+                                    Console.WriteLine(strInsert);
+
+                                    con = new MySqlConnection(this.conStr);
+                                    // コマンドを作成
+                                    cmd = new MySqlCommand(strInsert, con);
+                                    // オープン
+                                    cmd.Connection.Open();
+                                    // 実行
+                                    cmd.ExecuteNonQuery();
+                                    // クローズ
+                                    cmd.Connection.Close();
+
+                                    //インサート終了後、flagをtrueにする。
+                                    flag = true;
+
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                    }
+                    sr.Close();
+                    stream.Close();
+                }
+
+            }
+
         }
     }
 }
